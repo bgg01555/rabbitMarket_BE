fd503cc24c6d1b2a880c49faeb335f072594fca0
daonez <cyongchoi@gmail.com>
2022-02-11 22:10:06 +0900

    cors 미들웨어 추가

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ const express = require("express")
 const connect = require("./models")
 const port = 3000
 const app = express()
+const cors = require("cors")
 connect()
 
 //body 읽기
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use(cors())
 
 app.get("/", (req, res) => {
   res.send("Hello World")
