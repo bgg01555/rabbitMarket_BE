e7a244d0fab8db150f370ef00fc534c42585f34a
Sinclebear <sanghyukj@gmail.com>
2022-02-12 14:27:03 +0900

    👔 토큰이 없는 경우 로그인 요청하도록 메시지 변경

--- middlewares/auth-middleware.js
+++ middlewares/auth-middleware.js
@@ -31,8 +31,9 @@ module.exports = (req, res, next) => {
             });
     } catch (error) {
         // 토큰이 없거나, 유효하지 않은 토큰인 경우 이쪽으로 접근.
-        res.status(401).send({
-            errorMessage: '로그인이 필요한 페이지 입니다.',
+        res.send({
+            ok: false,
+            result: '로그인이 필요한 페이지 입니다.',
         });
         return;
     }
