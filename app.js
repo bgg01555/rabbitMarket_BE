eebe82c350b09e5c6cca0bb4ac5002e6b17d076d
daonez <cyongchoi@gmail.com>
2022-02-12 14:38:02 +0900

    main 브랜치 pull

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const connect = require("./models")
 const port = 3000
 const app = express()
 const cors = require("cors")
+const authMiddleware = require("./middlewares/auth-middleware")
 connect()
 
 const userRouter = require("./routes/user")
@@ -11,11 +12,10 @@ const userRouter = require("./routes/user")
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cors())
-app.use(requestMiddleware);
 
 // app.use("/api", [goodsRouter, cartsRouter]);
 // 여러 라우터를 사용할 경우 배열 형태로 배치
-app.use("/api", [userRouter]);
+app.use("/api", [userRouter])
 
 app.get("/", (req, res) => {
   res.send("Hello World1")
--- models/user.js
+++ models/user.js
@@ -1,20 +1,20 @@
 const mongoose = require("mongoose")
-const userSchema = new mongoose.Schema({
-  userId: {
-    type: String,
-    required: true,
-    unique: true,
+const userSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    nickname: {
+      type: String,
+      required: true,
+    },
   },
-  password: {
-    type: String,
-    required: true,
-  },
-  nickname: {
-    type: String,
-    required: true,
-    unique: true,
-  }
-},
   { timestamps: true } // createdAt, updatedAt 으로 Date형 객체 입력)
 )
 module.exports = mongoose.model("Users", userSchema)
